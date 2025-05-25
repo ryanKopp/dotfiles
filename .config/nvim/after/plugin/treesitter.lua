c2af049830fbd867259e40586ccce6e440b6c39f
Ryan Kopp <>
2025-05-24 23:17:49 -0700

    Add NixOS required config changes to nvim

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,13 +1,13 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "perl", "python" },
+  --ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "perl", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  auto_install = false,
 
   highlight = {
     -- `false` will disable the whole extension
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,12 @@
 require("ryan")
+
+-- nix attempted lsp parser dir fix
+require("nvim-treesitter.configs").setup({
+    parser_install_dir = vim.fn.stdpath("data") .. "/treesitter/parsers",
+    highlight = { enable = true },
+    indent = { enable = true },
+    -- Add other treesitter modules as needed
+})
+
+-- Ensure the parser directory is in Neovim's runtimepath
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/treesitter/parsers")
