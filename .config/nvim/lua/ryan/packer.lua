da7e4a35850ee6c7b282ec21cb268048d641af8a
Ryan Kopp <ryck574@gmail.com>
2025-06-05 22:27:50 -0700

    Remove trailing space

--- .config/nvim/lua/ryan/packer.lua
+++ .config/nvim/lua/ryan/packer.lua
@@ -11,7 +11,7 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use{'nvim-treesitter/nvim-treesitter', 
+    use{'nvim-treesitter/nvim-treesitter',
     run = function()
         local ts_update = require('nvim-treesitter.install').update({ with_sync = true})
         ts_update()
